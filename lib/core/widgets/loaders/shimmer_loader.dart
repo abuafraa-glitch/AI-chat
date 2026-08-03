1c106945da71be1f1459bba2c6fc98843876df90
raedthawaba <raed01560@gmail.com>
2026-08-04 01:51:14 +0300

    Add ShimmerLoader widget for loading animations
    Implement a ShimmerLoader widget with customizable colors and duration.

--- lib/core/widgets/loaders/shimmer_loader.dart
+++ lib/core/widgets/loaders/shimmer_loader.dart
@@ -1,1 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:ai_chat/core/extensions/build_context_extension.dart';
 
+class ShimmerLoader extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final Color? baseColor;
+  final Color? highlightColor;
+
+  const ShimmerLoader({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 1500),
+    this.baseColor,
+    this.highlightColor,
+  });
+
+  @override
+  State<ShimmerLoader> createState() => _ShimmerLoaderState();
+}
+
+class _ShimmerLoaderState extends State<ShimmerLoader> with SingleTickerProviderStateMixin {
+  late AnimationController _shimmerController;
+
+  @override
+  void initState() {
+    super.initState();
+    _shimmerController = AnimationController(vsync: this, duration: widget.duration)
+      ..repeat(reverse: false);
+  }
+
+  @override
+  void dispose() {
+    _shimmerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final baseColor = widget.baseColor ?? context.colorScheme.surfaceVariant.withOpacity(0.5);
+    final highlightColor = widget.highlightColor ?? context.colorScheme.surfaceVariant.withOpacity(0.2);
+
+    return AnimatedBuilder(
+      animation: _shimmerController,
+      builder: (context, child) {
+        return ShaderMask(
+          blendMode: BlendMode.srcATop,
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [baseColor, highlightColor, baseColor],
+              stops: const [0.1, 0.3, 0.5],
+              transform: _SlidingGradientTransform(slidePercent: _shimmerController.value),
+            ).createShader(bounds);
+          },
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
+
+class _SlidingGradientTransform extends GradientTransform {
+  final double slidePercent;
+
+  const _SlidingGradientTransform({required this.slidePercent});
+
+  @override
+  Matrix4? transform(Rect bounds, {TextDirection? textDirection}) {
+    return Matrix4.translationValues(bounds.width * slidePercent, 0.0, 0.0);
+  }
+}
