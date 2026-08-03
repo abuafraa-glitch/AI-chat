379ff84dc387f536de04ee6367db137af42af8de
raedthawaba <raed01560@gmail.com>
2026-08-03 23:06:03 +0300

    Add LoadingButton widget for loading states

--- lib/core/widgets/buttons/loading_button.dart
+++ lib/core/widgets/buttons/loading_button.dart
@@ -1,1 +1,41 @@
 
+import 'package:flutter/material.dart';
+import 'package:ai_chat/core/widgets/buttons/app_button.dart';
+
+class LoadingButton extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+  final bool isLoading;
+  final AppButtonType type;
+  final AppButtonSize size;
+  final bool fullWidth;
+  final Widget? icon;
+  final String? loadingText;
+  final Widget? loadingIcon;
+
+  const LoadingButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.isLoading = false,
+    this.type = AppButtonType.primary,
+    this.size = AppButtonSize.medium,
+    this.fullWidth = false,
+    this.icon,
+    this.loadingText,
+    this.loadingIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppButton(
+      text: isLoading ? loadingText ?? 'Loading...' : text,
+      onPressed: isLoading ? null : onPressed,
+      isLoading: isLoading,
+      type: type,
+      size: size,
+      fullWidth: fullWidth,
+      icon: isLoading ? loadingIcon : icon,
+    );
+  }
+}
