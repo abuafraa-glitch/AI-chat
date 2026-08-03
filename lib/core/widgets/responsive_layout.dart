9f0f45c1c1812812da95fe530691873407c7dd66
raedthawaba <raed01560@gmail.com>
2026-08-03 23:02:27 +0300

    Add ResponsiveLayout widget for adaptive UI
    Implemented a ResponsiveLayout widget to handle different screen sizes.

--- lib/core/widgets/responsive_layout.dart
+++ lib/core/widgets/responsive_layout.dart
@@ -1,1 +1,41 @@
 
+import 'package:flutter/material.dart';
+import 'package:ai_chat/core/constants/app_constants.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget? desktop;
+
+  const ResponsiveLayout({
+    super.key,
+    required this.mobile,
+    this.tablet,
+    this.desktop,
+  });
+
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.sizeOf(context).width < AppBreakpoints.tablet;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.sizeOf(context).width >= AppBreakpoints.tablet &&
+      MediaQuery.sizeOf(context).width < AppBreakpoints.desktop;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.sizeOf(context).width >= AppBreakpoints.desktop;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= AppBreakpoints.desktop && desktop != null) {
+          return desktop!;
+        } else if (constraints.maxWidth >= AppBreakpoints.tablet && tablet != null) {
+          return tablet!;
+        } else {
+          return mobile;
+        }
+      },
+    );
+  }
+}
