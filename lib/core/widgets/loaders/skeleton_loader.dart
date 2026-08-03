aa593621972ae6f63f1ee347018700486c0c213b
raedthawaba <raed01560@gmail.com>
2026-08-04 01:52:27 +0300

    Add SkeletonLoader widget for loading placeholders

--- lib/core/widgets/loaders/skeleton_loader.dart
+++ lib/core/widgets/loaders/skeleton_loader.dart
@@ -1,1 +1,44 @@
 
+import 'package:flutter/material.dart';
+import 'package:ai_chat/core/extensions/build_context_extension.dart';
+import 'package:ai_chat/core/theme/app_radius.dart';
+import 'package:ai_chat/core/widgets/loaders/shimmer_loader.dart';
+
+class SkeletonLoader extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final BorderRadiusGeometry? borderRadius;
+  final Color? baseColor;
+  final Color? highlightColor;
+
+  const SkeletonLoader({
+    super.key,
+    this.width,
+    this.height,
+    this.borderRadius,
+    this.baseColor,
+    this.highlightColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final effectiveBaseColor = baseColor ?? context.colorScheme.surfaceVariant.withOpacity(0.5);
+    final effectiveHighlightColor = highlightColor ?? context.colorScheme.surfaceVariant.withOpacity(0.2);
+
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: effectiveBaseColor,
+        borderRadius: borderRadius ?? AppRadius.sm,
+      ),
+      child: ShimmerLoader(
+        baseColor: effectiveBaseColor,
+        highlightColor: effectiveHighlightColor,
+        child: Container(), // Empty container to apply shimmer effect on
+      ),
+    );
+  }
+}
+
+
